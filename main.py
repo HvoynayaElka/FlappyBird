a1dacd0f62c078e7a7547b5dbbe42ccfc803257c
DimasFlr <151378934+DimasFlr@users.noreply.github.com>
2024-01-05 21:45:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,146 +1,70 @@
-import pygame
-import sys
 import os
+import sys
 
+import pygame
 
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def start_screen(): #пока что такой же как в уроке, потом нуэно будет менять
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    return image
-
-
-def load_level(filename=None):
-    filename = 'map.txt'
-    filename = "data/" + filename
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    max_width = max(map(len, level_map))
-
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-class Tile(pygame.sprite.Sprite): #создаёт все спрайты, кроме игрока
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(obstacles_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(TILE_WIDHT * pos_x, TILE_HEIGHT * pos_y)
-        self.mask = pygame.mask.from_surface(self.image)
-
-
-class Player(pygame.sprite.Sprite): #создаёт спрайт игрока
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_sprite)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(TILE_WIDHT * pos_x + 15, TILE_HEIGHT * pos_y + 5)
-        self.mask = pygame.mask.from_surface(self.image)
-
-
-def generate_level(level): # генерация уровня из файла
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                pass
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                new_player = Player(x, y)
-            elif level[y][x] == '*':
-                Tile('ground', x, y)
-    return new_player, x, y
-
-
-class Camera:
-    def __init__(self):
-        self.dx = 0
-
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        if obj.rect.x <= -TILE_WIDHT:
-            obj.rect.x += level_x * TILE_WIDHT
-
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-
-
+clock = pygame.time.Clock()
 pygame.init()
-WIDTH, HEIGHT = 1920, 1080
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption("Flappy bird")
+icon = pygame.image.load('images/bird/bird1.png')
+pygame.display.set_icon(icon)
 
-player = None
-camera = Camera()
+bg = pygame.image.load('images/bg.png')
+birds_fly = [pygame.image.load('images/bird/bird_2_1.png'), 
+             pygame.image.load('images/bird/bird_2_2.png'),
+             pygame.image.load('images/bird/bird_2_3.png'),
+             pygame.image.load('images/bird/bird_2_4.png'),
+             pygame.image.load('images/bird/bird_2_5.png'),
+             pygame.image.load('images/bird/bird_2_6.png'),
+             pygame.image.load('images/bird/bird_2_7.png'),
+             pygame.image.load('images/bird/bird_2_8.png'),
+             pygame.image.load('images/bird/bird_2_9.png'),
+             pygame.image.load('images/bird/bird_2_10.png'),
+             pygame.image.load('images/bird/bird_2_11.png'),
+             pygame.image.load('images/bird/bird_2_12.png'),
+             pygame.image.load('images/bird/bird_2_13.png'),
+             pygame.image.load('images/bird/bird_2_14.png')]
+player_anim_count = 0
 
+bg_x = 0
 
-obstacles_sprites = pygame.sprite.Group()
-player_sprite = pygame.sprite.Group()
+player_speed = 50
+player_y = 500
 
-TILE_WIDHT = TILE_HEIGHT = 100 #размер одной клетки
+bg_sound = pygame.mixer.Sound('sounds/les.mp3')
+bg_sound.play()
 
-tile_images = { #картинки спрайтов
-    'wall': pygame.transform.scale(load_image('box.png'), (TILE_WIDHT, TILE_HEIGHT)),
-    'ground': pygame.transform.scale(load_image('ground.png', -1), (TILE_WIDHT, TILE_HEIGHT))
-}
-player_image = load_image('mar.png') #картинка игрока
-player_image = pygame.transform.scale(player_image, (TILE_WIDHT * 2, TILE_HEIGHT))
+running = True
+while running:
+    player = birds_fly[player_anim_count]
+    colorkey = -1
+    colorkey = player.get_at((0, 0))
+    player.set_colorkey(colorkey)
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + 1920, 0))
+    screen.blit(player, (300, player_y))
 
-start_screen()
-timer = pygame.USEREVENT + 1 #таймер
-pygame.time.set_timer(timer, 7)
-while True:
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE] and player_y > 50:
+        player_y -= player_speed
+    elif player_y < 800:
+        player_y += player_speed / 5
+
+    if player_anim_count == 13:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
+
+    bg_x -= 2
+    if bg_x == -1920:
+        bg_x = 0
+
+    pygame.display.update()
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            terminate()
-        if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not player:
-            player, level_x, level_y = generate_level(load_level())
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            player.rect = player.rect.move(0, -50)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            player.rect = player.rect.move(0, 50)
-        if event.type == timer and player:
-            player.rect = player.rect.move(3, 0)
-    if player:
-        for sprite in obstacles_sprites:
-            if pygame.sprite.collide_mask(player, sprite):
-                terminate()
-        screen.fill((168, 216, 255))
-        camera.update(player)
-        for sprite in obstacles_sprites:
-            camera.apply(sprite)
-        camera.apply(player)
-    obstacles_sprites.draw(screen)
-    player_sprite.draw(screen)
-    pygame.display.flip()
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
+    clock.tick(20)
